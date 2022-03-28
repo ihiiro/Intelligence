974a66565863486e9ddd0395ec2fa931af636649
hiro <yassirelyakine@gmail.com>
2022-03-28 23:36:33 +0000

    working on the url extractor method

--- intel_engine/gather_intel.py
+++ intel_engine/gather_intel.py
@@ -1,27 +1,38 @@
+import requests
+
 class GatherIntel:
-    """Gather intelligence from the internet"""
+    """Gather intelligence from the internet."""
     def __init__(self, query, search_engine='https://www.google.com/'):
         self.query = query
         self.search_engine = search_engine
-        self.response = None
 
     def initialLookup(self):
         """Look something up using specified search engine(google by default)."""
-        import requests
         url = 'search?q='.join([self.search_engine, self.query])
-        self.response = requests.get(url)
-        return self.response
+        return requests.get(url)
 
     def extractUrls(self):
-        """Extract urls from search engine results page"""
-        if self.response != None: #true if .initialLookup() was called by object
-            if self.response.status_code == 200:
-                print('can be exracted')
-        else: #true if object did not call .initialLookup()
-            if self.initialLookup().status_code == 200:
-                print('can be extraced(had to call method)')
+        """Extract urls from search engine results page."""
+        response_text = self.initialLookup().text
+        url_list = list()
+        print(response_text[response_text.find('https')-20:response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https')-20:response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https'):response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https'):response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https'):response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https'):response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https'):response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https'):response_text.find('https')+len('https')+80])
+        response_text = response_text.split('https', 1)[1]
+        print(response_text[response_text.find('https'):response_text.find('https')+len('https')+80])
 
-gather_intel = GatherIntel('hefkdf')
+gather_intel = GatherIntel('the man')
 
-gather_intel.initialLookup()
 gather_intel.extractUrls()

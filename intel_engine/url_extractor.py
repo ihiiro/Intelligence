e9682db1c30fd522a8f098af0d5231031ad88340
hiro <yassirelyakine@gmail.com>
2022-03-29 22:49:04 +0000

    done with the url extractor module

--- intel_engine/url_extractor.py
+++ intel_engine/url_extractor.py
@@ -1,31 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from contextlib import contextmanager
+import shutup
 
-# selenium configs
+# shut those annoying warnings
+shutup.please()
+
+# configure selenium
 chromedriver_location = './chromedriver'
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(chromedriver_location, options=chrome_options)
 
-# main code
-class GatherIntel:
+class UrlExtractor:
     """Gather intelligence from the internet."""
     def __init__(self, query, search_engine='https://www.google.com/'):
         self.query = query
         self.search_engine = search_engine
         self.url_initial = '"https'
+        self.se_url = 'search?q='.join([self.search_engine, self.query])
+
+    @contextmanager
+    def seleniumCxtmanager(self, func):
+        """Setup and Teardown ops on selenium driver."""
+        driver.get(self.se_url)
+        try:
+            yield func()
+        finally:
+            driver.close()
+            driver.quit()
 
     def initialLookup(self):
         """Look something up using specified search engine(google by default)."""
-        url = 'search?q='.join([self.search_engine, self.query])
-        driver.get(url)
         return str(driver.page_source.encode('utf-8')) #return bytes in string format
 
     def constructUrl(self, start):
         """Construct urls from start string."""
         constructed_url = list()
         for c in start[1:]: # avoid the initial double quote
-            # only append valid url characters
+            # append valid url characters
             if c.isalnum() or c in ['-','.','_','~',':','/','?','#','[',']','@','!','$','&',"'",'(',')','*','+',',',';','=']:
                 constructed_url.append(c)
             else:
@@ -34,7 +47,8 @@ class GatherIntel:
 
     def extractUrls(self):
         """Extract urls from search engine results page."""
-        response_html = self.initialLookup()
+        with self.seleniumCxtmanager(self.initialLookup) as response_html:
+            pass
         url_list = list()
         for url in range(response_html.count(self.url_initial)):
             if url == 0:
@@ -43,7 +57,3 @@ class GatherIntel:
             response_html = response_html.split(self.url_initial, 1)[1]
             url_list.append(self.constructUrl(start=response_html[response_html.find(self.url_initial):]))
         return url_list
-
-gather_intel = GatherIntel(query='ukraine russia news')
-
-print(gather_intel.extractUrls())

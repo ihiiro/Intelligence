887473b5afd774dc19d8f87cf7bc541afa7e55b4
hiro <yassirelyakine@gmail.com>
2022-04-02 17:18:48 +0000

    cleanup

--- intel_engine/url_filter.py
+++ intel_engine/url_filter.py
@@ -4,11 +4,6 @@ def filterUrlList(url_list):
     """Keep only useful urls."""
     def filterFunc(url):
         """Check if url is up and valid."""
-        # for black in blacklist:
-        #     if black in url:
-        #         print(f'url: "{url}"')
-        #         print('Is blacklisted, filtered out.\n')
-        #         return False
         try:
             with requests.head(url) as request: #with to call request.close()
                 if str(request.status_code)[0] in ['4', '5']: #check if it's an error code(starts with 4 or 5)

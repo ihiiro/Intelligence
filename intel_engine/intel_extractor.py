3067b0e2604c04fb7e954a9867c0f93b677405e8
hiro <yassirelyakine@gmail.com>
2022-04-07 16:01:38 +0000

    extractImportants() is almost finished

--- intel_engine/intel_extractor.py
+++ intel_engine/intel_extractor.py
@@ -2,37 +2,53 @@ from bs4 import BeautifulSoup
 import selenium
 import pathlib
 
+def extractImportants(text, terms_found, debug=False):
+    """Extract important bits of text."""
+    importants = []
+    for term in terms_found:
+        temp_text = text
+        for ocrnc in range(temp_text.find(term)):
+            try:
+                for c in temp_text[temp_text.index(term):]:
+                    if c == '\n':
+                        importants.append('\n'*2)
+                        break
+                    importants.append(c)
+            except ValueError:
+                continue
+            temp_text = temp_text.split(term, 1)[1]
+    return ''.join(importants)
+
 def getUsefulText(url_list, driver, debug=False):
     """Return list of useful text from useful urls."""
     useful_text = []
     try:
-        keywords_file_location = f"{next(pathlib.Path('.').glob('**/keywords.txt'))}"
+        terms_file_location = f"{next(pathlib.Path('.').glob('**/terms.txt'))}"
     except StopIteration:
-        raise Exception('no keywords.txt file in directory.')
-    with pathlib.Path(keywords_file_location).open('r') as file:
+        raise Exception('no terms.txt file in directory.')
+    with pathlib.Path(terms_file_location).open('r') as file:
         file_content = file.read()
     if file_content.isspace() or len(file_content) == 0:
-        raise Exception('keywords.txt file is empty.')
-    keywords = file_content.split(',')
-    length_of_keywords_list = len(keywords)
+        raise Exception('terms.txt file is empty.')
+    terms = file_content.strip().lower().split(',') #stripping the files of EOF newlines
     for url in url_list:
-        if debug:
-            print(f'{url_list.index(url)+1}/{len(url_list)}, {len(useful_urls)} useful.\r', end='', flush=True)
+        terms_found = []
         try:
             driver.get(url)
         except selenium.common.exceptions.WebDriverException: #base webdriver exception
             continue
         try:
-            bs = BeautifulSoup(driver.find_element_by_xpath('html/body').text)
+            bs = BeautifulSoup(driver.find_element_by_xpath('html/body').get_attribute('innerHTML'))
         except selenium.common.exceptions.NoSuchElementException:
             continue
-        for unwanted in bs(['script', 'style', 'header', 'footer']):
+        for unwanted in bs(['style', 'header', 'footer', 'nav', 'script']):
             unwanted.decompose()
         bs_in_lowercase = bs.get_text().lower()
-        num_of_keywords_found = 0
-        for keyword in keywords:
-            if keyword.lower() in bs_in_lowercase:
-                num_of_keywords_found += 1
-        if num_of_keywords_found > 0:
-            useful_urls.append(f'{url}:\n{bs.get_text()}\n')
-    return useful_urls
+        for term in terms:
+            if term in bs_in_lowercase:
+                terms_found.append(term)
+        if len(terms_found) > 0:
+            useful_text.append(extractImportants(text=bs_in_lowercase, terms_found=terms_found))
+        if debug:
+            print(f'{url_list.index(url)+1}/{len(url_list)}, {len(useful_text)} useful.\r', end='', flush=True)
+    return useful_text
